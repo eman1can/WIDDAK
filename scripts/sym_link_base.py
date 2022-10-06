de9f55cc99d836437bb687dd6ddaef81bc690175
Ethan Wolfe <ethan.wolfe212@gmail.com>
2022-10-06 14:36:23 -0700

    Update Sym Links to add local worlds and a simple user interface.

---
+++ scripts/sym_link_base.py
@@ -0,0 +1,95 @@
+# Detect Minecraft Installation Location
+from os import getcwd, listdir, symlink, remove
+from os.path import exists, join
+from common import get_mincraft_location
+
+save_location = join(get_minecraft_location(), 'saves')
+
+links = {}
+if exists('.sym_link_data'):
+    with open('.sym_link_data', 'r') as file:
+        for x in file:
+            v, k = x.strip().split(',')
+            links[k] = v
+else:
+    print('No sym link data found.')
+
+# Verify All Links
+dead_links = 0
+for link_dst, link_src in links.items():
+    if not exists(link_src):
+        remove(link_dst)
+        dead_links += 1
+if dead_links > 0:
+    print(f'Removed {dead_links} dead links')
+
+
+class SymLinkManager:
+    def __init__(self):
+        self._save_path = save_location
+        self._base_path = getcwd()
+        self._links = links
+
+    def save_links(self):
+        with open('.sym_link_data', 'w') as file:
+            for k, v in self._links.items():
+                file.write(','.join((v, k)) + '\n')
+
+    def make_sym_link(self, world_path, world_name):
+        # Make relative paths absolute
+        if not world_path.startswith(self._base_path):
+            world_path = join(self._base_path, world_path)
+        dst = join(self._save_path, world_name)
+        if exists(dst):
+            remove(dst)
+        symlink(world_path, dst)
+        self._links[dst] = world_path
+        return world_path, dst
+
+    def delete_sym_link(self, world_path, world_name):
+        dst = join(self._save_path, world_name)
+        if not exists(dst):
+            print(dst)
+            print('Sym Link Doesn\'t Exist')
+        else:
+            remove(dst)
+            self._links.pop(dst)
+
+    def is_link(self, link_path):
+        absolute_path = join(self._base_path, link_path)
+        for link_src, link_dst in self._links:
+            if link_src == absolute_path:
+                return True
+        return False
+
+    def make_past_code_links(self):
+        for year in listdir('past-code'):
+            for project_name in listdir(join('past-code', year)):
+                if project_name == '__MACOSX':
+                    continue
+                project_path = join(getcwd(), 'past-code', year, project_name)
+                if not project_path.endswith('maps'):
+                    project_path = join(project_path, 'maps')
+                if not exists(project_path):
+                    continue
+                map_files = [x for x in listdir(project_path) if x != '__MACOSX']
+                if len(map_files) == 0:
+                    continue
+                for ix, map_file in enumerate(map_files):
+                    self.make_sym_link(join(project_path, map_file), f'{year} {project_name} Map {ix + 1}')
+
+    def remove_past_code_links(self):
+        for year in listdir('past-code'):
+            for project_name in listdir(join('past-code', year)):
+                if project_name == '__MACOSX':
+                    continue
+                project_path = join(getcwd(), 'past-code', year, project_name)
+                if not project_path.endswith('maps'):
+                    project_path = join(project_path, 'maps')
+                if not exists(project_path):
+                    continue
+                map_files = [x for x in listdir(project_path) if x != '__MACOSX']
+                if len(map_files) == 0:
+                    continue
+                for ix in range(len(map_files)):
+                    self.delete_sym_link(project_path, f'{year} {project_name} Map {ix + 1}')
---
+++ scripts/sym_link_manager.py
@@ -0,0 +1,88 @@
+from os import listdir
+from os.path import exists, isdir, join
+
+from scripts.sym_link_base import SymLinkManager
+
+print('Welcome to the sym link manager')
+
+dialog = """Enter a command
+\t0. Create Past Code Sym Links
+\t1. Remove Past Code Sym Links
+\t2. Add World Sym Link
+\t3. Remove World Sym Link
+\t4. Exit
+
+\t>> """
+
+base = SymLinkManager()
+
+
+def get_worlds(add):
+    world_files = listdir('worlds')
+    worlds = []
+    for file in world_files:
+        world_path = join('worlds', file, 'raw')
+        if not exists(world_path) or not isdir(world_path):
+            continue
+        if add and base.is_link(world_path):
+            continue
+        worlds.append((world_path, file))
+    return worlds
+
+
+def select_world(worlds):
+    world_dialog = 'Select a World\n'
+    for ix, (world_path, world_name) in enumerate(worlds):
+        world_dialog += f'\t{ix}. {world_name}\n'
+    world_dialog += '\n\t>> '
+    world_command = input(world_dialog)
+
+    try:
+        index = int(world_command.strip())
+    except ValueError:
+        print('Invalid Index')
+        return None
+    return index
+
+
+def get_selected_world(add=True):
+    index = None
+    worlds = get_worlds(add)
+
+    if len(worlds) == 0:
+        print('No Worlds to Sym Link\n')
+        return None
+
+    while index is None:
+        index = select_world(worlds)
+
+    return worlds[index]
+
+
+while True:
+    command = input(dialog)
+    try:
+        command_index = int(command.strip())
+    except ValueError:
+        print('Invalid Command\n')
+        continue
+
+    if command_index == 4:
+        print('Goodbye!')
+        break
+    elif command_index == 0:
+        base.make_past_code_links()
+        print('Sym Links Created')
+    elif command_index == 1:
+        base.remove_past_code_links()
+        print('Sym Links Removed')
+    elif command_index == 2:
+        selected_world = get_selected_world()
+        if selected_world is not None:
+            base.make_sym_link(*selected_world)
+    elif command_index == 3:
+        selected_world = get_selected_world(False)
+        if selected_world is not None:
+            base.delete_sym_link(*selected_world)
+
+base.save_links()
