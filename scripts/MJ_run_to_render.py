c5579361dd5631db58d04a92302b1bb19724b5b8
Emily Gavrilenko <egavrile@calpoly.edu>
2022-12-01 15:57:34 -0800

    Move and rename vox_visualizer to scripts

--- scripts/MJ_run_to_render.py
+++ scripts/MJ_run_to_render.py
@@ -3,9 +3,25 @@ import subprocess
 from subprocess import Popen, PIPE
 import shutil
 from time import perf_counter
-from examples import visualize_vox as vv
 
+# Path Fixing Code - Must Be First
+import sys
+from os import getcwd, environ, chdir
+from os.path import split
 
+script_path = getcwd()
+sys.path.append(script_path)
+while not script_path.endswith('WIDDAK'):
+    script_path = split(script_path)[0]
+    chdir(script_path)
+if 'PYTHONPATH' in environ:
+    if script_path + ';' not in environ['PYTHONPATH']:
+        environ['PYTHONPATH'] += script_path + ';'
+else:
+    environ['PYTHONPATH'] = script_path
+# End Path Fixing Code
+
+import vox_to_minecraft as vm
 
 def run_markov():
     # Get current working directory
@@ -99,23 +115,21 @@ def run_multiple_markov(num: int):
 
     os.chdir(s_cwd)
 
-    #TODO - Convert to minecraft
-    # Send to minecraft
-
-
 def run_to_minecraft():
-    run_multiple_markov(1)
+    # run_multiple_markov(1)
 
-    filepath5 = 'MarkovJunior/multiple_outputs/ModernHouseOutput0.vox'
+    filepath5 = 'MarkovJunior/output/ModernHouse.vox'
+    filepath6 = 'MarkovJunior/resources/SavedVoxels/ApartemazementsTiny_1481160288.vox'
 
-    vv.clear_build_area()
-    template = vv.create_template_from_vox(filepath5, 'Modern House',
-                                        'modern_house', 'jungle')
+    # vm.clear_build_area()
+    # template = vm.create_template_from_vox(filepath5, 'Modern House',
+    #                                     'modern_house', 'jungle')
+    template = vm.create_template_from_vox(filepath6, 'Adobe Village',
+                                        'apartemazements', 'forest')
     location = [70, 130, 70]
-    vv.visualize_vox_template(template, location)
+    vm.visualize_vox_template(template, location)
 
 
 if __name__ == "__main__":
-    # run_multiple_markov(4)
     run_to_minecraft()
 
---
+++ scripts/vox_to_minecraft.py
@@ -0,0 +1,75 @@
+# Path Fixing Code - Must Be First
+import sys
+from os import getcwd, environ, chdir
+from os.path import split
+
+from gdpc.direct_interface import place_block_at, place_blocks
+
+script_path = getcwd()
+sys.path.append(script_path)
+while not script_path.endswith('WIDDAK'):
+    script_path = split(script_path)[0]
+    chdir(script_path)
+if 'PYTHONPATH' in environ:
+    if script_path + ';' not in environ['PYTHONPATH']:
+        environ['PYTHONPATH'] += script_path + ';'
+else:
+    environ['PYTHONPATH'] = script_path
+# End Path Fixing Code
+
+from glm import ivec2, ivec3
+from amulet.api.block import Block
+from gdpc.vector_util import addY, vecString
+from gdpc.util import eprint
+from gdpc.interface import getBuildArea, getWorldSlice
+from gdpc.template import Template
+from src.vox import VoxFile
+
+CLEAR_AREA_RADIUS = 30
+CLEAR_AREA = ivec2(CLEAR_AREA_RADIUS, CLEAR_AREA_RADIUS)
+
+
+def clear_build_area():
+    build_area = getBuildArea()
+    world_slice = getWorldSlice(build_area.toRect())
+
+    air = Block('minecraft', 'air')
+    to_replace = []
+    for p in build_area.loop():
+        block = world_slice.get_block_data_at(p)
+        if block.base_name != 'air':
+            to_replace.append(p)
+    place_block_at(air, ivec3(0, 0, 0), to_replace)
+
+
+def visualize_vox_template(template, location=None):
+    # Get the build area
+    buildArea = getBuildArea()
+    buildRect = buildArea.toRect()
+
+    # Check whether the build area is large enough
+    if any(buildRect.size < CLEAR_AREA):
+        eprint(f"The build area rectangle is too small! Its size needs to be at least {vecString(CLEAR_AREA)}")
+        sys.exit(1)
+
+    # Set the center of the template build area
+    if (location):
+        center = addY(ivec2(location[0], location[2]), location[1]) 
+    else:
+        center = addY(ivec2(0, 0), 120)
+    print(center)
+
+    blocks = []
+    for ix, iy, iz, relation, block in template.loop():
+        blocks.append((block, ivec3(ix, iy, iz)))
+        if len(blocks) > 100:
+            place_blocks(blocks, ivec3(buildArea.x1, buildArea.y1, buildArea.z1))
+            blocks = []
+    place_blocks(blocks, ivec3(buildArea.x1, buildArea.y1, buildArea.z1))
+
+def create_template_from_vox(vox_filepath, name, key, biome='forest'):
+    # Read in the VOX file
+    vox = VoxFile.from_file(vox_filepath)
+    # Make the Template
+    template = Template.from_vox_model(name, vox.get_model(), vox.get_color_palette(), key, biome)
+    return template
